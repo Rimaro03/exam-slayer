1539aab038d0b32ccc3cd43dfe39d1d21062777e
Davide <davide.tonno.dt@gmail.com>
2024-06-26 22:24:55 +0200

    Update Vec2.java

--- src/main/java/org/project/utils/Vec2.java
+++ src/main/java/org/project/utils/Vec2.java
@@ -21,21 +21,6 @@ public class Vec2 {
     }
 
     /**
-     * Clamps a Vec2 between a minimum and maximum value
-     *
-     * @param value The Vec2 to clamp
-     * @param min   The minimum value
-     * @param max   The maximum value
-     * @return A new Vec2 that is the clamped version of the input Vec2
-     */
-    public static Vec2 clamp(Vec2 value, Vec2 min, Vec2 max) {
-        return new Vec2(
-                Math.max(min.getX(), Math.min(max.getX(), value.getX())),
-                Math.max(min.getY(), Math.min(max.getY(), value.getY()))
-        );
-    }
-
-    /**
      * Adds another Vec2 to this Vec2
      *
      * @param other The other Vec2 to add
@@ -65,25 +50,6 @@ public class Vec2 {
         return new Vec2(x * scalar, y * scalar);
     }
 
-    /**
-     * Divides this Vec2 by a scalar
-     *
-     * @param scalar The scalar to divide by
-     * @return A new Vec2 that is the quotient of this Vec2 and the scalar
-     */
-    public Vec2 divide(float scalar) {
-        return new Vec2(x / scalar, y / scalar);
-    }
-
-    /**
-     * Calculates the dot product of this Vec2 and another Vec2
-     *
-     * @param other The other Vec2 to calculate the dot product with
-     * @return The dot product of this Vec2 and the other Vec2
-     */
-    public float dot(Vec2 other) {
-        return x * other.x + y * other.y;
-    }
 
     /**
      * Calculates the magnitude of this Vec2
@@ -104,24 +70,6 @@ public class Vec2 {
         return mag != 0 ? new Vec2(x / mag, y / mag) : new Vec2(0, 0);
     }
 
-    /**
-     * Calculates the distance between this Vec2 and another Vec2
-     *
-     * @param other The other Vec2 to calculate the distance to
-     * @return The distance between this Vec2 and the other Vec2
-     */
-    public float distance(Vec2 other) {
-        return subtract(other).magnitude();
-    }
-
-    /**
-     * Creates a copy of this Vec2
-     *
-     * @return A new Vec2 that is a copy of this Vec2
-     */
-    public Vec2 copy() {
-        return new Vec2(x, y);
-    }
 
     /**
      * Checks if this Vec2 is equal to another Vec2
